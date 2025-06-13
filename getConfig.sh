c1d6a82b2c56a4c2fdc30032a86349237368197b
Jessica Prendi <jessica.prendi@cern.ch>
2025-06-13 14:33:11 +0200

    upload of basic hlt config file

---
+++ getConfig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+    hltGetConfiguration /dev/CMSSW_15_0_0/GRun \
+       --globaltag 150X_dataRun3_HLT_v1 \
+       --data \
+       --unprescale \
+       --output minimal \
+       --max-events -1 \
+       --eras Run3_2024 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_1_1_xml \
+        --input " " \
+       > hltData.py
+
+
+cat <<@EOF >> hltData.py
+
+process.hltOutputMinimal.outputCommands = [
+    'drop *',
+    'keep GlobalObjectMapRecord_hltGtStage2ObjectMap_*_HLTX',
+    'keep edmTriggerResults_*_*_HLTX',
+    'keep triggerTriggerEvent_*_*_HLTX' 
+]
+
+
+process.options.numberOfThreads = 1
+
+@EOF
+
+edmConfigDump hltData.py > dump.py
+
