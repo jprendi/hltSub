9e1ec3dc5063c0789788b558e73277a4a434bf5c
Jessica Prendi <jessica.prendi@cern.ch>
2025-06-27 12:28:13 +0200

    two config files that change the conditions

---
+++ getLCPedconfig.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -ex
+
+    hltGetConfiguration /dev/CMSSW_15_0_0/GRun \
+       --globaltag 150X_dataRun3_HLT_v1 \
+       --data \
+       --unprescale \
+       --output minimal \
+       --max-events -1 \
+       --eras Run3_2024 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_1_1_xml \
+        --input " " \
+       > hltDataLCPed.py
+
+
+cat <<@EOF >> hltDataLCPed.py
+
+process.GlobalTag.toGet = cms.VPSet(
+  cms.PSet(record = cms.string("EcalPedestalsRcd"),
+           tag = cms.string("EcalPedestals_prompt"),
+           connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+          )
+)
+
+
+process.GlobalTag.toGet.append(
+  cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
+           tag = cms.string("EcalLaserAPDPNRatios_prompt_v3"),
+           connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+          )
+)
+
+process.hltOutputMinimal.outputCommands = [
+    'drop *',
+    'keep GlobalObjectMapRecord_hltGtStage2ObjectMap_*_HLTX',
+    'keep edmTriggerResults_*_*_HLTX',
+    'keep triggerTriggerEvent_*_*_HLTX' 
+]
+
+
+process.options.numberOfThreads = 1
+
+@EOF
+
+edmConfigDump hltDataLCPed.py > dumpLCPed.py
+
